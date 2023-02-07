3900b4841ccaf37f9faf9aa573c2adb66e1de02a
Ivan Lougan <robinio123@gmail.com>
2023-02-07 17:39:05 +0000

    using map method and displaying values on click in a console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const bookPrices = [2.4, 3, 4.25, 6, 8]
+  const [allPeople, setAllPeople] = useState([
+    { name: "Dan", age: 32 },
+    { name: "Bob", age: 30 },
+    { name: "Kri", age: 23 }
+  ]);
+
+  const handleClick = (clickValue) => {
+    console.log(clickValue);
+
+    if (clickValue == 8) {
+      alert("expensive stuff");
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Map method</h1>
+
+      {bookPrices.map(( price, index ) => {
+        return <h2 key={index} onClick={() => handleClick(price)}> This book is {price}</h2>
+      })}
+
+      {allPeople.map( (singlePerson, index) => {
+        return <Person key={index} 
+          runOnClick={() => handleClick(singlePerson)} 
+          personsName={singlePerson.name} 
+          personsAge={singlePerson.age} />        
+      })}
+      
     </div>
   );
+}
+
+const Person = (props) => {
+
+  return (
+    <div onClick={props.runOnClick}> 
+      <h3>{props.personsName}</h3>
+      <p>{props.personsAge}</p>
+    </div>
+  )
+
 }
 
 export default App;
